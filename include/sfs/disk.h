2bb3be667e83b715764321b91420b0d475564359
rudra <rudradesai200@gmail.com>
2020-12-09 15:04:46 +0530

    Added comments for disk layer

--- include/sfs/disk.h
+++ include/sfs/disk.h
@@ -1,58 +1,94 @@
-// disk.h: Disk emulator
+/**
+ * @file disk.h
+ * @author University of Notre Dame (https://www.nd.edu/)
+ * @brief Contains the implementation of disk layer to support File System.
+ * @date 2017
+ * 
+ */
 
 #pragma once
 
 #include <stdlib.h>
 
+/**
+ * @brief Disk class
+ * Implements Disk abstraction that enables emulation of a disk image.
+ * Used by file system to access and make changes to the disk.
+ */
 class Disk {
 private:
-    int	    FileDescriptor; // File descriptor of disk image
-    size_t  Blocks;	    // Number of blocks in disk image
-    size_t  Reads;	    // Number of reads performed
-    size_t  Writes;	    // Number of writes performed
-    size_t  Mounts;	    // Number of mounts
+    int	    FileDescriptor;                                         /** File descriptor of disk image @hideinitializer*/
+    size_t  Blocks;	                                                /** Number of blocks in disk image @hideinitializer*/
+    size_t  Reads;	                                                /** Number of reads performed @hideinitializer*/
+    size_t  Writes;	                                                /** Number of writes performed @hideinitializer*/
+    size_t  Mounts;	                                                /** Number of mounts @hideinitializer*/
 
-    // Check parameters
-    // @param	blocknum    Block to operate on
-    // @param	data	    Buffer to operate on
-    // Throws invalid_argument exception on error.
+    /** 
+     * @brief check if the block is within valid range
+     * @param blocknum index of the block into the free block bitmap
+     * @param data data buffer
+     * @return void function; returns nothing. throws invalid_argument on error
+     */
     void sanity_check(int blocknum, char *data);
 
 public:
-    // Number of bytes per block
-    const static size_t BLOCK_SIZE = 4096;
+    const static size_t BLOCK_SIZE = 4096;                          /** Number of bytes per block */
     
-    // Default constructor
+    /**
+     * @brief constructor of Disk class
+     * @return an instance of Disk class
+     */
     Disk() : FileDescriptor(0), Blocks(0), Reads(0), Writes(0), Mounts(0) {}
     
-    // Destructor
+    /**
+     * @brief destructor of Disk class
+     * @return returns nothing; deletes the Disk object 
+     */
     ~Disk();
 
-    // Open disk image
-    // @param	path	    Path to disk image
-    // @param	nblocks	    Number of blocks in disk image
-    // Throws runtime_error exception on error.
-    void open(const char *path, size_t nblocks);
+    /**
+     * @brief opens the disk image
+     * @param path path to the disk image
+     * @param nblocks number of blocks in the disk image
+     * @return void function; returns nothing. throws runtime_error exception on error.
+     */
+    void    open(const char *path, size_t nblocks);
 
-    // Return size of disk (in terms of blocks)
-    size_t size() const { return Blocks; }
+    /**
+     * @brief check size of disk
+     * @return size of disk in terms of blocks
+     */
+    size_t  size() const { return Blocks; }
 
-    // Return whether or not disk is mounted
-    bool mounted() const { return Mounts > 0; }
+    /**
+     * @brief check if the disk has been mounted
+     * @return true if the disk has been mounted; false otherwise
+     */
+    bool    mounted() const { return Mounts > 0; }
 
-    // Increment mounts
-    void mount() { Mounts++; }
+    /** 
+     * @brief mount the disk
+     * @return void function; returns nothing
+     */
+    void    mount() { Mounts++; }
 
-    // Decrement mounts
-    void unmount() { if (Mounts > 0) Mounts--; }
+    /** 
+     * @brief unmount the disk
+     * @return void function; returns nothing
+     */
+    void    unmount() { if (Mounts > 0) Mounts--; }
 
-    // Read block from disk
-    // @param	blocknum    Block to read from
-    // @param	data	    Buffer to read into
-    void read(int blocknum, char *data);
+    /**
+     * @brief read from disk
+     * @param blocknum block to read from
+     * @param data data buffer to write into
+     */
+    void    read(int blocknum, char *data);
     
-    // Write block to disk
-    // @param	blocknum    Block to write to
-    // @param	data	    Buffer to write from
-    void write(int blocknum, char *data);
+    /**
+     * @brief write to disk
+     * @param blocknum block to write into
+     * @param data data buffer to read from
+     */
+    void    write(int blocknum, char *data);
 };
--- src/library/disk.cpp
+++ src/library/disk.cpp
@@ -1,4 +1,10 @@
-// disk.cpp: disk emulator
+/*!
+ * @file disk.cpp
+ * @author University of Notre Dame (https://www.nd.edu/)
+ * @brief Implementation of disk.h functions
+ * @date 2017
+ * 
+ */
 
 #include "sfs/disk.h"
 
@@ -10,26 +16,41 @@
 #include <unistd.h>
 
 void Disk::open(const char *path, size_t nblocks) {
+    /** <dl class="section implementation"> */
+    /** <dt> Implementation details </dt>*/
+    /** </dl> */
+
+    /**- Open Filedescriptor  */
     FileDescriptor = ::open(path, O_RDWR|O_CREAT, 0600); 
+    
+    /**- Check if FileDescriptor is valid */
     if (FileDescriptor < 0) {
     	char what[BUFSIZ];
     	snprintf(what, BUFSIZ, "Unable to open %s: %s", path, strerror(errno));
     	throw std::runtime_error(what);
     }
 
+    /**- Allocated blocks */
     if (ftruncate(FileDescriptor, nblocks*BLOCK_SIZE) < 0) {
     	char what[BUFSIZ];
     	snprintf(what, BUFSIZ, "Unable to open %s: %s", path, strerror(errno));
     	throw std::runtime_error(what);
     }
 
+    /**- Initialize the disk */
     Blocks = nblocks;
     Reads  = 0;
     Writes = 0;
 }
 
 Disk::~Disk() {
+    /** <dl class="section implementation"> */
+    /** <dt> Implementation details </dt>*/
+    /** </dl> */
+
+    /**- Check if FileDescriptor is set */
     if (FileDescriptor > 0) {
+        /**- If set, print the required information and close. */
     	printf("%lu disk block reads\n", Reads);
     	printf("%lu disk block writes\n", Writes);
     	close(FileDescriptor);
@@ -38,8 +59,13 @@ Disk::~Disk() {
 }
 
 void Disk::sanity_check(int blocknum, char *data) {
-    char what[BUFSIZ];
+    /** <dl class="section implementation"> */
+    /** <dt> Implementation details </dt>*/
+    /** </dl> */
 
+    char what[BUFSIZ]; /**- reason for exit. */
+
+    /**- Check if block num is valid or not.  */
     if (blocknum < 0) {
     	snprintf(what, BUFSIZ, "blocknum (%d) is negative!", blocknum);
     	throw std::invalid_argument(what);
@@ -50,6 +76,7 @@ void Disk::sanity_check(int blocknum, char *data) {
     	throw std::invalid_argument(what);
     }
 
+    /**- Check if data pointer is valid */
     if (data == NULL) {
     	snprintf(what, BUFSIZ, "null data pointer!");
     	throw std::invalid_argument(what);
@@ -57,37 +84,53 @@ void Disk::sanity_check(int blocknum, char *data) {
 }
 
 void Disk::read(int blocknum, char *data) {
+    /** <dl class="section implementation"> */
+    /** <dt> Implementation details </dt>*/
+    /** </dl> */
+
+    /**- sanity_check blocknum and data */
     sanity_check(blocknum, data);
 
+    /**- reposition read/write file offset and check validity */
     if (lseek(FileDescriptor, blocknum*BLOCK_SIZE, SEEK_SET) < 0) {
     	char what[BUFSIZ];
     	snprintf(what, BUFSIZ, "Unable to lseek %d: %s", blocknum, strerror(errno));
     	throw std::runtime_error(what);
     }
 
+    /**- read the FileDescriptor for BLOCK_SIZE */
     if (::read(FileDescriptor, data, BLOCK_SIZE) != BLOCK_SIZE) {
     	char what[BUFSIZ];
     	snprintf(what, BUFSIZ, "Unable to read %d: %s", blocknum, strerror(errno));
     	throw std::runtime_error(what);
     }
 
+    /**- Increment reads */
     Reads++;
 }
 
 void Disk::write(int blocknum, char *data) {
+    /** <dl class="section implementation"> */
+    /** <dt> Implementation details </dt>*/
+    /** </dl> */
+
+    /**- sanity_check blocknum and data */
     sanity_check(blocknum, data);
 
+    /**- reposition read/write file offset and check validity */
     if (lseek(FileDescriptor, blocknum*BLOCK_SIZE, SEEK_SET) < 0) {
     	char what[BUFSIZ];
     	snprintf(what, BUFSIZ, "Unable to lseek %d: %s", blocknum, strerror(errno));
     	throw std::runtime_error(what);
     }
 
+    /**- write the BLOCK_SIZE data to the FileDescriptor  */
     if (::write(FileDescriptor, data, BLOCK_SIZE) != BLOCK_SIZE) {
     	char what[BUFSIZ];
     	snprintf(what, BUFSIZ, "Unable to write %d: %s", blocknum, strerror(errno));
     	throw std::runtime_error(what);
     }
 
+    /**- increment writes */
     Writes++;
 }
